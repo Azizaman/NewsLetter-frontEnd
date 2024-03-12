77271c91f46a4ed2b81cbf9083ea0c63219372a8
Azizaman <azizamanaaa97@gmail.com>
2024-03-12 16:12:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { NavBar } from './components/NavBar'
 import { Hero } from './components/Hero'
 import { Analytics } from './components/Analytics'
 import { NewsLetter } from './components/NewsLetter'
-import {NextUIProvider} from "@nextui-org/react";
 import {Footer} from './components/Footer'
 import { Cards } from './components/Cards'
 
